8873e8125c37e9102c820466eef9d9300cee29e1
vitalik94 <vitalij_petrov_95@mail.ru>
2021-05-26 19:27:35 +0300

    homework2

---
+++ hw2/src/com/company/Task5.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Task5 {
+
+    public static void main(String[] args) {
+
+        final int r = 5;
+
+        double s;
+        double l;
+
+        s = Math.PI * Math.pow(r, 2);
+        l = 2 * Math.PI * r;
+
+        System.out.println("площадь круга " + s);
+        System.out.println("длина окружности " + l);
+
+    }
+
+}
