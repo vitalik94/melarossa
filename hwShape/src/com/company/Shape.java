a554f2c0d1d6782a66714e6d7357ab3cc0076c3a
vitalik94 <vitalij_petrov_95@mail.ru>
2021-06-13 16:12:24 +0300

    homeworkPerson

---
+++ hwShape/src/com/company/Shape.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class Shape {
+
+    private String color;
+
+    public Shape() {
+
+        this.color = color;
+
+    }
+
+    public Shape(String color) {
+
+        this.color = color;
+
+    }
+
+    public void setColor(String color) {
+
+        this.color = color;
+
+    }
+
+    public String getColor() {
+
+        return color;
+
+    }
+
+    public void draw() {
+
+        System.out.println("фигура");
+
+    }
+
+}
